9a47d751241d289e489d85d558d2c8313476e0e9
algodon <179851883+algodondev@users.noreply.github.com.>
2024-12-19 22:15:54 -0600

    feat: created computer choice function

---
+++ main.js
@@ -0,0 +1,18 @@
+function getComputerChoice(){
+    const number = Math.floor(Math.random() * 3) + 1;
+    let choice
+
+    if (number == 1){
+        choice = 'rock'
+    } else if (number == 2){
+        choice = 'paper'
+    } else if (number == 3){
+        choice = 'scissors'
+    }
+
+    return choice;
+}
+
+
+
+
